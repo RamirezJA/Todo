9f78f9545018eddb2e9a4ffda5fb966c8652c8fe
Jose Ramirez <26813102+RamirezJA@users.noreply.github.com>
2022-11-09 19:58:42 -0800

    functions

---
+++ app.js
@@ -0,0 +1,33 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+//Functions
+function addTodo(event) {
+  //prevent form from submitting
+  event.preventDefault()
+  //to do Div
+  const todoDiv = document.createElement('div')
+  todoDiv.classList.add('todo')
+  //create li
+  const newTodo = document.createElement('li')
+  newTodo.innerText = 'hey'
+  newTodo.classList.add('todo-item')
+  todoDiv.appendChild(newTodo)
+  //Check Mark Button
+  const completedButton = document.createElement('button')
+  completedButton.innerHTML = '<i class="fas fa-check"></i>'
+  completedButton.classList.add('complete-btn')
+  todoDiv.appendChild(completedButton)
+  //Trash Button
+  const trashButton = document.createElement('button')
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+  trashButton.classList.add('complete-btn')
+  todoDiv.appendChild(trashButton)
+  //Append to list
+  todoList.appendChild(todoDiv)
+}
