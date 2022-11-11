88af2e0637e0c8cbf653328746e908115c08e5bd
Jose Ramirez <26813102+RamirezJA@users.noreply.github.com>
2022-11-10 22:33:30 -0800

    animations

--- app.js
+++ app.js
@@ -40,7 +40,11 @@ function deleteCheck(e) {
   //Delete todo
   if (item.classList[0] === 'trash-btn') {
     const todo = item.parentElement
-    todo.remove()
+    //Animation
+    todo.classList.add('fall')
+    todo.addEventListener('transitionend', function () {
+      todo.remove()
+    })
   }
   //Check Mark
   if (item.classList[0] === 'complete-btn') {
