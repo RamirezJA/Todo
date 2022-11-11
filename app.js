cbb936220ae0bf9f52a6e299e1f2bf3184101e92
Jose Ramirez <26813102+RamirezJA@users.noreply.github.com>
2022-11-10 22:12:20 -0800

    delete func/toggle checkmark

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list')
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo)
+todoList.addEventListener('click', deleteCheck)
 
 //Functions
 function addTodo(event) {
@@ -32,4 +33,18 @@ function addTodo(event) {
   todoList.appendChild(todoDiv)
   //To do INput nothing
   todoInput.value = ''
+}
+
+function deleteCheck(e) {
+  const item = e.target
+  //Delete todo
+  if (item.classList[0] === 'trash-btn') {
+    const todo = item.parentElement
+    todo.remove()
+  }
+  //Check Mark
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement
+    todo.classList.toggle('completed')
+  }
 }
